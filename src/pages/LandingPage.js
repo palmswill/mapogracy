1f3f793b47f2f3f2658e15ddc7c48b9d6952356b
William Liu <liuwilly4@gmail.com>
2022-03-19 18:29:56 -0400

    adding axios

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,7 +8,7 @@ const Landingpage = () => {
   return (
     <>
     <h1>Landing</h1>
-    {/* <Arcmap height="500px" voteList={[{cords:[-118.244, 34.052]}]}/> */}
+    <Arcmap height="500px" voteList={[{cords:[-118.244, 34.052]}]}/>
 
     </>)
 }
