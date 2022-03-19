704894fc58bd6c3c60b3334579bf980424f8c059
William Liu <liuwilly4@gmail.com>
2022-03-19 00:30:17 -0400

    auth0 login function with login/logout buttons

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import { AppBar,Box,Toolbar,IconButton,Typography,Button } from '@mui/material';
+import Usernavsection from './UserNavSection';
 
 const Navbar = () => {
+
   return (
     <>
+     <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ marginRight:"auto" }}>
+            Mapocracy
+          </Typography>
+          <Usernavsection/>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </>
   );
 }
