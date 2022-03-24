bb0f5e666255e31452e246c632f14897c0d8ffbe
William Liu <liuwilly4@gmail.com>
2022-03-24 14:59:07 -0400

    reset modal open

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
       },
     },
   });
-  const [modalOpen, setModelOpen] = useState(true);
+  const [modalOpen, setModelOpen] = useState(false);
 
   const toggleModal = () => {
     setModelOpen((prevState) => !prevState);
