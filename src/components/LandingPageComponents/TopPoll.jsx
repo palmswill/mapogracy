ce1ca3a9ab5435d56edd105d865c93d64266c946
William Liu <liuwilly4@gmail.com>
2022-03-26 16:05:21 -0400

    added main display hover

--- src/components/LandingPageComponents/TopPoll.jsx
+++ src/components/LandingPageComponents/TopPoll.jsx
@@ -1,9 +1,11 @@
-import { Typography, Paper} from "@mui/material";
-import React,{useState,useEffect } from "react";
-import axios from "axios"
+import { Typography, Paper } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Mainpollcard from "./PollCards/MainPollCard";
+import { useNavigate } from "react-router-dom";
 
 const TopPoll = () => {
+  const navigate = useNavigate();
 
   const [newestPoll, setNewestPoll] = useState([]);
 
@@ -12,11 +14,9 @@ const TopPoll = () => {
       .get("http://mapocracy-api.azurewebsites.net/poll?order=new")
       .then((res) => res.data)
       .then((result) => {
-        return setNewestPoll(result[1])});
+        return setNewestPoll(result[1]);
+      });
   }, []);
-
-
-
 
   return (
     <>
@@ -24,10 +24,24 @@ const TopPoll = () => {
         What's New?
       </Typography>
       <Paper
+        className="main-card"
+        onClick={() => navigate(`/polls/${newestPoll.id}`)}
         sx={{
           minHeight: "400px",
+          boxSizing: "border-box",
           padding: "2%",
+          border: "black solid 1px",
+
           position: "relative",
+          "&:hover ": {
+            boxSizing: "border-box",
+
+            outline: "purple solid 2px",
+            border: "purple solid 1px",
+
+            cursor: "pointer",
+            outlineOffset: "-10px",
+          },
         }}
       >
         {newestPoll.id && <Mainpollcard poll={newestPoll} />}
